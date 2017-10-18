dc85bd7e5512f9363dc58a5741525f8c5145e45c
Neil Murphy <wnmurphy@gmail.com>
2017-10-18 11:19:42 -0700

    Adds photo of cabin to home page.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import cabin from './../img/cabin.jpg';
 class Home extends Component {
 
   render() {
@@ -8,10 +8,10 @@ class Home extends Component {
         <h1>Home</h1>
         <p>This is the Home view.</p>
         <p>This is the welcome page, basically.</p>
+        <img className="cabin-photo" src={cabin} alt="Photo of cabin in winter."/>
       </div>
     )
   }
-
 }
 
 export default Home;
